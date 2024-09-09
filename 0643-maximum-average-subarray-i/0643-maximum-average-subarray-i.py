b46c02ad3d4eeff16fa71c6abbdec44a05d28c21
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-09-09 18:11:52 -0400

    Time: 867 ms (83.39%), Space: 28.3 MB (83.47%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,6 +1,8 @@
-class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
-        if len(nums) == 1:
+#Notes got deleted while refreshing :(
+#Passed all test cases but only beats 5%, good progress but a lot of hard coding
+'''
+My code:
+if len(nums) == 1:
             return float(nums[0])
         if k == 1:
             nums.sort()
@@ -20,4 +22,17 @@ class Solution:
             
 
         return maxAvg
+'''
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if k == 1:
+            return max(nums)
+    
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
         
+        for i in range(k, len(nums)):
+            window_sum = window_sum - nums[i-k] + nums[i]
+            max_sum = max(max_sum, window_sum)
+        
+        return max_sum / k
