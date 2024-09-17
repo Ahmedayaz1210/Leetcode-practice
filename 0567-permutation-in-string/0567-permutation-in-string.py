ff084fac2ca8760fc739c530f7a70b22e926e49d
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-09-17 00:32:12 -0400

    Time: 63 ms (55.67%), Space: 16.6 MB (59.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -24,30 +24,58 @@ PLAN:
 - See if occurrences for all letters match, if so return true
 - Else shrink window from left and take out that letter's occurrence from s2's hashmap and expand window from the right, appending that letter in s2 hash map
 - In the end return false because we never found a permutation
+
+My code:
+        s1_hashmap = {}
+        s2_hashmap = {}
+        for s in range(len(s1)):
+            s1_hashmap[s1[s]] = 1 + s1_hashmap.get(s1[s],0)
+        print(s1_hashmap)
+        
+        l = 0
+        for r in range(len(s2)):
+            
+            if s2[r] not in s1_hashmap:
+                if s2[l] in s2_hashmap:
+                    s2_hashmap[s2[l]] = s2_hashmap.get(s2[l]) - 1
+                l += 1
+            else:
+                s2_hashmap[s2[r]] = 1 + s2_hashmap.get(s2[r],0)
+                print(s2_hashmap)
+                if s1_hashmap == s2_hashmap:
+                    return True
+
+        if s1_hashmap == s2_hashmap:
+                return True
+        return False
+
+
+EVALUATE: 
+- My code was almost correct, once again had logic error from start so implementing one small missing thing got confusing
+- The trick was to add right pointer letter from the start and then check if any letter's value in s2 is greater than s1's if so take it out and move right
+- Another trick was rather than comparing the two hashmaps, since we were validating window, we had to match window size with s1's length
+
+Time: O(n) n is length os s2
+Space complexity: In worst case, will have all letters so O(26) == O(1)
 '''
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1_hashmap = {}
         s2_hashmap = {}
         
-        # Populate the s1_hashmap with character counts from s1
         for s in s1:
             s1_hashmap[s] = 1 + s1_hashmap.get(s, 0)
         
         l = 0
         for r in range(len(s2)):
-            # Add the current character to the s2_hashmap
             s2_hashmap[s2[r]] = 1 + s2_hashmap.get(s2[r], 0)
             
-            # If the current character count in s2_hashmap exceeds the count in s1_hashmap
-            # or if it's not in s1_hashmap, we need to shrink the window from the left
             while s2_hashmap.get(s2[r], 0) > s1_hashmap.get(s2[r], 0):
                 s2_hashmap[s2[l]] -= 1
                 if s2_hashmap[s2[l]] == 0:
-                    del s2_hashmap[s2[l]]  # Remove the character from hashmap if the count is 0
+                    del s2_hashmap[s2[l]]  
                 l += 1
             
-            # If the window size matches the size of s1, check for permutation match
             if (r - l + 1) == len(s1):
                 if s1_hashmap == s2_hashmap:
                     return True
