20dc01fb2add56dbb4fa6bd601d45fcfdb14f1f8
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-09-23 19:38:06 -0400

    Time: 35 ms (76.22%), Space: 16.6 MB (35.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,3 +1,39 @@
+'''
+UNDERSTAND:
+- Input: Two sorted LL, list1 and list2
+- Both LL are sorted
+- We need to merge them together and make it into one merged sorted LL
+- Loop over both lists and compare their corresponding position nodes and place the smaller one before larger one in the merged list
+- list1 and list2 same length? no
+- If one is bigger: when the smaller LL finishes, you append the larger ones all remaining nodes as is to the merged list
+- Empty lists? return empty lists
+- Time constraint?
+- Space constraint?
+- Is node.val only going to be ints? Yes
+- How big can each int be?
+- How big can one LL be? 
+- Both always going to be sorted
+- How do we handle same node values? Doesn't matter
+- Not always going to be corresponding nodes
+
+MATCH:
+- Obv LL question
+
+PLAN:
+- Create merged_list 
+- Loop over the lists until one of them reaches null
+- Will be a while loop because we need to handle our pointers based off of a comparison statement
+- Place pointers on both lists
+- Check if one is smaller than the other
+- Append the smaller one to the merged_list and increment it
+- In the end if either of the two lists have any nodes remaining append to the merged_list
+
+EVALUATE:
+- I was able to solve the question, the only thing I messed up was I used "or" instead of "and" in while loop because we only need to check if we have looped over one of them
+- n is the length of list1 and m is the length of list2
+- Time complexity: O(n+m)
+- Space Complexity: O(1)
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,10 +45,8 @@ class Solution:
         current = merged_list
         l1 = list1
         l2 = list2
-        m = []
+        
         while l1 != None and l2 != None:
-            m.append(current.val)
-            print(m)
             if l1.val < l2.val:
                 current.next = l1
                 l1 = l1.next
