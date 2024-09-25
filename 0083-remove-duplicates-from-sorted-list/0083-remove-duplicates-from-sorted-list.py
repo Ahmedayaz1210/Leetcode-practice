6db2fbce83907ba7d26111661c558cc57614c7cb
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-09-25 18:04:11 -0400

    Time: 31 ms (95.23%), Space: 16.5 MB (33.3%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -1,3 +1,36 @@
+'''
+UNDERSTAND:
+- Input: Given a sorted LL
+- Guaranteed the list we get is sorted
+- Have to remove all duplicates from the LL
+- Any nodes with repeated values get taken out and only one of their kind remains
+- Return back the sorted list with duplicates removed
+- Empty LL? Return back nothing? None?
+- If no dups exist, return as is
+- How long can out LL be? 0 - 300
+- Our all nodes just going to have int values? Yes
+- How big can each integer be inside a node? -100 to 100
+- Time Constraint?
+- Space Constraint?
+- Do I return back head of the new LL?
+- LL is only going to be singly?
+
+MATCH:
+- Hashing in this problem
+- Helps us find duplicate items
+- It's about finding duplicate values rather than nodes
+- Hash set will be handy
+
+PLAN:
+- Start our curr pointer at head
+- Check if it's already in the hashset, if so we skip over it
+- Else we append it to hash set and keep track of it's previous one and make it point to current's next
+
+EVALUATE:
+- Was able to solve it in 20 minutes!
+- Time Complexity: O(n)
+- Space Complexity: O(n)
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
