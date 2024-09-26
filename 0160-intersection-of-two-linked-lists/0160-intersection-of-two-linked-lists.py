0e32015c53028588f17e28e92f3d33c03646e37f
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-09-26 19:44:57 -0400

    Time: 88 ms (19.45%), Space: 27.3 MB (24.46%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -7,17 +7,33 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
-        a_pointer = headA
-        b_pointer = headB
-        record_a = set()
+        if not headA or not headB:
+            return None  # If either list is empty, there can't be an intersection
+
+        # Initialize two pointers for each list
+        pointerA = headA
+        pointerB = headB
+
+        # Traverse both lists
+        while pointerA != pointerB:
+            # Move to the next node or switch to the head of the other list
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+
+        # If they meet, return the intersection node, otherwise return None
+        return pointerA
+
+        # a_pointer = headA
+        # b_pointer = headB
+        # record_a = set()
         
         # Traverse list A and record the nodes
-        while a_pointer:
-            record_a.add(a_pointer)
-            a_pointer = a_pointer.next
+        # while a_pointer:
+        #     record_a.add(a_pointer)
+        #     a_pointer = a_pointer.next
 
-        while b_pointer:
-            if b_pointer in record_a:
-                return b_pointer
-            b_pointer = b_pointer.next
-        return None
+        # while b_pointer:
+        #     if b_pointer in record_a:
+        #         return b_pointer
+        #     b_pointer = b_pointer.next
+        # return None
