ea4a3cffc6546d081f28f21e56979863620c96a6
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-10-15 14:37:03 -0400

    Time: 35 ms (53.98%), Space: 16.6 MB (48.42%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,3 +1,30 @@
+'''
+UNDERSTAND:
+- Input: Given root nodes ot two binary trees p and q
+- Have to see if both of the Binary trees are exactly identical to each other or not
+- Identical: has the exact same structure of nodes and those nodes have the exact same value, so it the root node of p has a value of 1, q's root node needs to have a value of 1 as well, so on
+- Output: A boolean value, if trees are same: return else if not not at any point return False instantly
+- Is this a BT? Yes
+- By structure it means that nodes have to be in the same position in both trees? Yes
+- Tree is represented by nodes? Yes
+- How many nodes can we have? [0,100]
+- If we have empty tree, do we return True? Yes
+- Each node has an int value? Yes
+- How big? -10^4 to 10^4
+- A BT can have between 0-2 nodes? Yes
+
+MATCH:
+- We can use recursion to loop over the the trees and see if every node matches
+
+PLAN:
+- Using recursion
+- Base Case: if not root: return True
+- Checking for the same value: 
+    - if p.val != q.val: return False
+- Checking for the same structure:
+
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,13 +33,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        if not p and not q: 
             return True
-        if not p or not q or p.val != q.val:
+        if not p or not q:
             return False
-        else:
-            lSol = self.isSameTree(p.left, q.left)
-            if lSol:
-                return self.isSameTree(p.right, q.right)
-            else:
-                return False
+        
+        
+        
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left,q.left) and p.val == q.val
+        
+        
