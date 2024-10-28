5cfbdf7b712c7c051eab3f18c47bf4c636da600c
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-10-28 18:20:02 -0400

    Time: 0 ms (100%), Space: 16.6 MB (12.01%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,36 +1,28 @@
 '''
 UNDERSTAND:
-- Input; Given root of a BT
-- BT has between 0-2 nodes
-- The given BT is in a form of root, left, right
-- Have to find inorder traversal
-- Inorder Traversal is return the BT in a form left, root, right so left node comes first then root and then right
-- We have to go to depth of the tree for each node such as in example 2 left most node is 4
-- Output: return the tree's root node but this time tree is arranged in inorder traversal, returning nodes.val in a list
+- Input: Given root of a BT
+- Have to return back the root the binary tree in a list of integers by changing the structure of the BT into inorder traversal
+- Inorder traversal is: Left node, Root node and Right node
+- A BT can have 0-2 descendants for each root node
 - Empty Tree: return empty tree
-- Tree has only one node: return just that
-- TC?
-- SC?
-- How many nodes can we have in total? 0 to 100
-- How big is the value of each node? -100 to 100
+- 1 Node: return that node
+- Num of nodes: [0.100]
+- Node.val: -100 to 100
 
 MATCH:
-- Since we are finding nodes from the root's depth we can use recursion or iteration
-- I will go with recursion
+- Recursion
 
 PLAN:
-- create global list function
-- create a helper function to go in depths
-- base case: if no root then just simple return because we are not appending null values
-- call this on left
-- append the root val 
-- call on right
-- return list
-
-EVALUATE:
-- I was trying to append left and right both into list but we only need to append root because when the base case hits both left and right will be none so in that case we jus return root.val and then form there it builds up
-- TC: O(n)
-- SC: O(n)
+- Base case: if not root: return []
+- Can utilize a global array to keep track of our traversal
+- Because of the global array can create a helper function
+- Helper function takes in a root node for parameter
+- Base Case: if not root: return (don't have to return an empty list because we are using a global list now)
+- helper function (root.left)
+- array.append(root.val)
+- helper function (root.right)
+- return array
+- return array
 '''
 # Definition for a binary tree node.
 # class TreeNode:
@@ -40,13 +32,21 @@ EVALUATE:
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        li = []
-        def dfs(root):
-            if not root:
+        if not root:
+            return []
+
+        result = []
+        def dfs(node):
+            
+            if not node:
                 return
 
-            dfs(root.left)
-            li.append(root.val)
-            dfs(root.right)
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+            return result
+        
         dfs(root)
-        return li
+        
+        return result
+        
