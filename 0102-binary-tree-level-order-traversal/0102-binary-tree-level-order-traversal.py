3d0695e4ec25efc07323d70a5f4a7cdf6bce5a46
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-10-30 19:32:19 -0400

    Time: 3 ms (25.29%), Space: 17.3 MB (23.22%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,39 @@
+'''
+UNDERSTAND:
+- Input: Given root of a BT
+- Have to return a list of the nodes level by level, so a sublist at each level has to be appended to our final list
+Output: Return the final list with all sublists containing integer values
+- LOT: At each level of the BT, we return left node(s) + right node(s)
+- TC?
+- SC?
+- How many nodes can we have? [0,2000]
+- Node.val? -1000 to 1000
+- 1 node? return a sublist of that node
+- No nodes return empty list
+
+MATCH:
+- Recursion
+
+PLAN:
+- Base case: if not root return []
+- Initialize Result list
+- Helper function to loop over left and right side 
+- We want to go to next level when we have recursed over both sides
+- One side can't leave the other behind, we can't have a mismatch
+- Helper function(left, right):
+- if not root: return
+
+MISTAKE:
+- Up until now I was using recursion but if you technically look, we should use recursion in DFS problems when we are looking to go into the depths of the question but here we want all the nodes at each level so it would be BFS
+- I have never implemented BFS so using neetcode video
+
+PLAN: 
+- Use a deque for FIFO structure so root gets appended first in the result list
+- Before moving to the next level we have to empty out the queue
+- When you pop the node from queue, add it's children to the queue
+- TC and SC: O(n)
+
+'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,25 +42,21 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        
-        result = []
-        queue = deque([root])
-        
-        while queue:
-            level_size = len(queue)
-            current_level = []
-            
-            for _ in range(level_size):
-                node = queue.popleft()
-                current_level.append(node.val)
-                
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            
-            result.append(current_level)
-        
-        return result
+        res = []
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
