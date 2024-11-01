7edf874f3dbea1e4a4ca5921a0a5616aa23b5a57
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-10-31 21:28:35 -0400

    Time: 4 ms (10.2%), Space: 16.6 MB (54.25%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,60 @@
+'''
+UNDERSTAND:
+- Input: Given root of a BT
+- Have to look at the BT as I am looking at it from the right side and only return those nodes
+- So seems like returning all of the right most nodes
+- Output: return a list of integer node values 
+- TC?
+- SC?
+- How many nodes? [0,100]
+- Node.val? -100 to 100 inclusive
+- If we have no nodes we return an empty list
+- Poor examples: Actually it is not just the right nodes, if we don't have right nodes, we can still see the left ones from the right side
+- So now we need to check if right doesn't exist but left does, we append left
+
+
+MATCH:
+- DFS because we are trying to go from top to bottom exploring just the right nodes
+- Recursion
+
+PLAN:
+- Initialize the result list
+- Base case: if not root return []
+- Helper DFS function (node):
+    - if not node: return
+    - result.append(node.val)
+    - DFS(node.right)
+- DFS(root)
+- return result
+- Now I need to check if right exists, then append it but if for the same level, right doesn't exist, append the left one
+- Examples was bad, I figured we need to use BFS since at each level we are exploring both right and left (if right doesn't exist we would need this)
+- USING BFS
+
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            for i in range(qLen):
+                node = q.popleft()
+                if i == qLen - 1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
