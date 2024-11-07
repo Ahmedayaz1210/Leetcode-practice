d44d51af558a6d7f876b2fa7f473dd157b13ea1a
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-11-07 11:37:32 -0500

    Time: 3 ms (35.27%), Space: 20.1 MB (15.98%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,6 +1,23 @@
 '''
 UNDERSTAND:
-- 
+- Input: Given root of a BST
+- Have to find the kth smallest value in our tree
+- Kth smallest value means if for example k = 1, we have to find first smallest value which is aka smallest or the minimum value or if k = number of nodes then obviously the max value
+- Return kth smallest as an integer
+- TC?
+- SC?
+- How many nodes? n
+- Node.val: >= 0 and <= 10^4
+- how big can k be? 1 <= k <= n <= 104
+- Can it be bigger than num of nodes? No
+
+MATCH:
+- We can use BFS to store nodes at each level
+- DFS can be used to
+- Queue for BFS
+
+PLAN:
+- BF approach can be to store the nodes in a list, then sort them and return k-1 position element. TC: O(n log n), SC: O(n)
 '''
 # Definition for a binary tree node.
 # class TreeNode:
@@ -10,21 +27,19 @@ UNDERSTAND:
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        values = []
-        def bfs(root):
-            if not root:
+        self.li = []
+        print(self.li)
+        def dfs(node):
+            if not node:
                 return
-            queue = [root]
-            while queue:
-                node = queue.pop(0)
-                values.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+
+            self.li.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+            return self.li
+        dfs(root)
+        self.li.sort()
         
-        # Collect values using BFS
-        bfs(root)
-        # Sort values and return kth (1-indexed)
-        values.sort()
-        return values[k-1]
+        return self.li[k-1]
+            
+
