22a86af005515431754f1fcf991f9580c2cc350a
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-11-07 20:13:50 -0500

    Time: 0 ms (100%), Space: 20.1 MB (16.75%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -8,23 +8,15 @@ UNDERSTAND:
 - SC?
 - How many nodes? n
 - Node.val: >= 0 and <= 10^4
-- how big can k be? 1 <= k <= n <= 104
-- Can it be bigger than num of nodes? No
+- how big can k be? 1 <= k <= n <= 10^4
+- Can k be bigger than num of nodes? No
 
 MATCH:
-- We can use BFS to store nodes at each level
-- DFS can be used to
-- Queue for BFS
+- For BF we can use dfs and recursion to do this
 
-PLAN:
-- BF approach can be to store the nodes in a list, then sort them and return k-1 position element. TC: O(n log n), SC: O(n)
-'''
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+PLAN BF:
+- BF approach can be to use basic DFS, store node values in a list, at the end sort the list and return k-1, TC: O(n log n), SC:O(n)
+- But have to do this in O(n) TC
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         self.li = []
@@ -41,5 +33,32 @@ class Solution:
         self.li.sort()
         
         return self.li[k-1]
-            
 
+PLAN OPTIMAL:
+- If you technically look BST is already sorted because left is always smaller and right is always bigger
+- We can use In order traversal where we traverse over a tree in left -> root -> right manner since this will give us a sorted order from the start because left most has ot be smallest and right most biggest
+- Now also we don't need to store all the nodes in a list, rather we can have a counter of nodes we are looping over, just one integer value
+- This works because since we are using in order traversal, obviously we will be looping over in ascending order since the start and when our counter reacher the kth node, we can just return it because it is the kth smallest in our loop
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.counter = 0
+        def dfs(node):
+            if not node:
+                return 
+            left = dfs(node.left)
+            if left is not None:
+                return left
+            self.counter += 1
+            if self.counter == k:
+                return node.val
+            right = dfs(node.right)
+            return right
+        return dfs(root)
+        
