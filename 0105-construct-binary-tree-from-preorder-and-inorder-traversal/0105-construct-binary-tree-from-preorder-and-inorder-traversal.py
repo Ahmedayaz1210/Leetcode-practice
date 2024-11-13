123e5cd831954a1f62e972c46195408f5c247465
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2024-11-13 16:07:49 -0500

    Time: 3 ms (91.98%), Space: 18.3 MB (79.46%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -29,6 +29,21 @@ EVALUATE:
 - Couldn't solve this question, it was tricky and hard, had to observe preorder and inorder closely to find the pattern
 - TC: O(n^2) because for each preorder root we have to find it in inorder. Better explanation if confused: https://claude.ai/chat/bc64ebc3-556b-4c0d-852b-eb7816a9c3ff
 - SC: O(n^2) because O(n) for recursion stack and O(n) in worst case for storing our sublists like a skewed right tree where each sublist is just in right call. For better understanding check: https://chatgpt.com/g/g-cKXjWStaE-python/c/67323a90-c404-8003-9710-24f1a0e26dfd
+
+BF Code:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
+
+
+OPTIMAL APPROACH O(n):
+- We cam use a hashmap to store our indices rather than use .index() because hashmap has a retrieval time of O(1) on average and you don't have to search the array again and again making it O(n).
+- For space we can just pass back indices which can act as two pointers or a new window we will be working with. this way we don't take extra space building up new array in each level of recursion. So sliding window pattern can help here
 '''
 # Definition for a binary tree node.
 # class TreeNode:
@@ -38,11 +53,19 @@ EVALUATE:
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
+        self.hashmap = {num: i for i, num in enumerate(inorder)}
+        
+        self.pre_idx = 0
+        def dfs(l, r):
+            if l > r:
+                return None
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+            mid = self.hashmap[root_val]
+            root.left = dfs(l, mid - 1)
+            root.right = dfs(mid + 1, r)
+            return root
+        return dfs(0, len(inorder)-1)
+    
+            
