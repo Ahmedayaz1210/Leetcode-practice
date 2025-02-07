7ba9e32410ad2afdb955414be95329ddac1c5392
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2025-02-07 18:12:46 -0500

    Time: 47 ms (29.13%), Space: 18.2 MB (34.9%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,57 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+'''
+UNDERSTAND:
+- Input: Given root of an undirected connected graph as a node
+- Have to traverse over that graph and create a deep copy of it
+- A deep copy is basically an exact clone of the graph but it uses entirely different memory, it doesn't point to the same memory as original
+- Output: Return root node of the deep copy graph
+- Node class concists of an int val and a list neighbors which keeps track of all the neighbors a node is connected to
+- Each node in the given graph has the same value as it's position, 1 indexed, so node 1 has value 1 and so on
+- So we have to make new nodes but with same value and pretty much connected to the same neighbors clones
+- How big can the graph be? [0,100]
+- 1 <= Node.val <= 100
+- Is each node going to have a unique value? Yes
+- The Graph is connected and all nodes can be visited starting from the given node.
+- Empty graph return empty graph
+- A node's neighbors are represented in an adjacency list where each sub list in the input is neighbors of the current index node we are on
+- TC? SC?
+
+MATCH:
+- So what do we have to do here? Have to loop the graph and create new copies
+- But how do we loop? We take the given node and explore it's all neighbors
+- Let's use DFS to explore each path 
+- Addtionally let's use a hashmap to create a new node and store original node's value inside it
+- Then we explore neighbors
+
+PLAN:
+- A hashmap to create the clones and remember which ones we have already created
+- Looping over the graph by taking the first node and visiting it's each neighbor through DFS
+- Now for each node we are on, we create a new node at the original node's position and we append the original's value into the new node
+
+'''
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
+            copy = Node(node.val)
+
+            oldToNew[node] = copy
+
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+
+            return copy
+
+        return dfs(node) if node else None
+        
