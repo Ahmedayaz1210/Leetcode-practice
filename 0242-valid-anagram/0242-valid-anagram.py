5ee3b5c874a5eb2a6c3794a05cf76cdd4139495d
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-01-10 10:50:17 -0500

    Time: 15 ms (42.25%), Space: 19.6 MB (5.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -40,5 +40,19 @@ class Solution:
         
 
         
-
+# * So basically in this question we are given two strings and we are checking if both have the exact **same characters** which would mean they have to be the **same Length**. 
+# * Seems like another question where I could use a hashset to remember the characters in the first array and then match with the second.
+# * Also seems like the same question as contains duplicate, so this is a pattern of Arrays and hashing.
+# * Let's use hashset as extra memory to optimize the time complexity: **Time complexity and Space Complexity will be : O(n)**
+# * Would a hashset work for this because a hashset does not contain any duplicate values and here there could be duplicates.
+# * let's try solving with a hashset
+# * Solution passed 43/48 test cases but still good attempt..
+# * **Another solution**
+# * saw this in discussions but what if i sort both arrays and then compare each value, if both are the same then it's true else false
+# * **Another solution**
+# * We can use hashmaps to keep count of each character's occurrences and compare them
+# * so in python we can use dictionaries
+# * Here time and space complexity are both O(n)
+# * what if interviewer wants us to make space complexity O(1) so we don't use extra space. then we just use sorted method but even sorting algorithms take space complexity.
+# * even a hasmap can atmost take 26 characters after that it is repetition
         
