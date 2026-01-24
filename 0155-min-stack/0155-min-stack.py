711abc4973f4fb892181d625a1088780437dd092
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-01-24 11:17:14 -0500

    Time: 3 ms (82.99%), Space: 22.5 MB (26.46%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -31,3 +31,34 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+'''
+* Understand
+* More of a design problem where we have to implement different functions.
+* Given 4 functions, push takes an element and appends it in the stack.
+* Pop remove top element of stack
+* top just shows the top element but doesn't remove
+* getMin returns the minimum element in stack
+* Tricky part is everything has to be done in O(1) time which is easy for push pop and top but confusing for getting the minimum which makes me think we can't always have min at top because that's sorting and defeats constant time. maybe we need to keep track of min at all times?
+* How big can val be? check constraints
+* How many times can we call these functions? and do we only call on non empty stacks? check constraints
+
+* Match:
+* Easy to tell it's a stack problem.
+* For push and pull we can use python's default stack functions
+* For getting the top we can just look at stack's [-1] because this give's the last element which in our case would be the top one.
+* I am thinking maybe we can have a global min var and each time push function is called, we can update it and getMin just returns that, but this has a slight problem, if we remove the min, how do we determine the next min in constant time?
+* So we will be using extra space, as in the time at stack where at each element we add, at that point this was the min, and once we remove from top we remove it's memory from extra space by removing last element
+
+* Plan:
+* Init function: Initialize the stack and extra space
+* push function, use python's function to append into the stack and if it's first element add it to extra space, else keep going and compare with last element in extra space, of course in code this would get handled itself so dont have to write if else statement
+* pop, use python's function to pop out the top and remove last element from extra space to erase its memory
+* top retrieve -1 element from stack
+* getMin return last element in extra space
+
+Evaluate:
+* TC: O(1)
+* SC: O(n) worst case you append everything and don't pop
+* I was able to write the code myself but took me a hint and bit to understand we can keep track of min at memory at each point, so would give myself 80% of the credit. 
+'''
