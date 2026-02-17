64845e80b845745de67f276e7194597af28f007d
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-02-17 14:37:52 -0500

    Time: 10 ms (85.36%), Space: 19.5 MB (5.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,52 +1,37 @@
 '''
-UNDERSTAND:
-- Input:
- - Given a string
-- Have to find the longest substring without repeating characters
-- It is a substring not a subsequence so it has to be conitguous
-- As soon as a char repeats, we stop looking at that substring and move on
-- Output:
- - return the longest non repeating substring length, so returning an INTEGER
-Questions:
- - Length of given string: 0 to 5 * 10 ^4
- - What are the characters in the string, symbols, integers, letters? letters, digits, symbols and spaces
-- Edge case: if length is 0: we return 0, if 1 we return 1
-- If we have a space: we trim it out
+Understand:
+- Given a string as input, we need to find the longest substring so that within that substring each char which is present, occurs only once.
+- Duplicate chars tells me that we can use a hashset over here
+- Examples make the problem pretty straightforward
+- How long can s be? 0 to 5 ^ 10^4
+- s only consists of alphabets? nope symbols and spaces can be in there too
 
-MATCH:
-- Substring with a contiguous sequence tells that we will have to create a window 
-- Window will be shrinking and growing, looking whether a duplicate is encountered or not
-- To find duplicate: use hashMap, to keep track of the occurences of each character within the substring
+Match:
+- Finding the longest substring can be done using a sliding window where we create the window with two pointers and keep expanding until chars are unique
+- For keeping track of unique chars we can use a hashset so it ensures nothing is repeated
 
-PLAN:
-- Keep a dictionary to track all occuring characters with their positions
-- Keep a longest substring counter
- - Reason we put position in value is if we encounter a duplicate we move left pointer to one over the first occurence of our repeated character
-- Keep a left pointer at 0, put it in dictionary
-- Start a loop from 1 - end of string
-- right starts at 1
-- keep a current window size counter
-- If right doesn't exist in dictionary, add it with it's position, increment right, increment current window size counter
-- else if it does, take element from dictionary and it's position, pop it out of dicitionary, do max between longest substring counter and current one and move left pointer to one over the position of that occuring element, but it's first occurence.
-return longest substring counter
+Plan:
+- l = 0, hashset = {} and max = 0
+- run a loop over s with r
+    - now as long as s[r] keeps existing in hashset, we keep moving window from left side inwards, so remove s[l] from hashset and move l over
+- now once s[r] doesn't exist, append this new element and check if max needs to get updated
+- return max
+
+- So a key thing which kept confusing me was why do we run a while loop and not if, the reason is if we have abcbde, we get abc and when r gets on b now we want to start next string at c right so we keep deleting from hashset until left pointer comes to c, now left is on c, c is only element in hashset and r keeps moving and we get cbde
+
+Evaluate:
+- Was a bit tricky to be honest, got the logic couldn't get code, so 50% credit only
+- TC: O(n) looping once
+- SC: O(n) in worst if we store the whole s in hashset
 '''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
+        l, hashset, maxlen = 0, set(), 0
+        for r in range(len(s)):
+            while s[r] in hashset:
+                hashset.remove(s[l])
+                l += 1
+            hashset.add(s[r])
+            maxlen = max(maxlen, r - l + 1)
+        return maxlen
         
-        longestSub = 0
-        l = 0
-        hashMap = {}
-        
-        for r, char in enumerate(s):
-            if char in hashMap and hashMap[char] >= l:
-                # second statement in conditional basically checks char is occuring within
-                # window so we don't need to remove it, later we can update it's new position
-                l = hashMap[char] + 1
-            else:
-                longestSub = max(longestSub, r - l + 1)
-            
-            hashMap[char] = r
-        
-        return longestSub
