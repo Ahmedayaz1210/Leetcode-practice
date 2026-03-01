d8c6c73d14d0f39fa5c97ce9025adc0146e94d94
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-03-01 10:28:39 -0500

    Time: 11 ms (79.26%), Space: 19.5 MB (29.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,37 +1,36 @@
 '''
 Understand:
-- Given a string as input, we need to find the longest substring so that within that substring each char which is present, occurs only once.
-- Duplicate chars tells me that we can use a hashset over here
-- Examples make the problem pretty straightforward
-- How long can s be? 0 to 5 ^ 10^4
-- s only consists of alphabets? nope symbols and spaces can be in there too
+- Given a string and we have to find the longest contg sequence as a substring where in that subarray no alphabets are repeated twice, how long can substring be with unique alphas
+- I mean in the worst case it can be O(26)
+- We have to return the len of that longest substring
+- There could be multiple substrings which are same longest len
+- Actually its not only english letters, it could be symbols and spaces as well so not exactly O(26)
 
 Match:
-- Finding the longest substring can be done using a sliding window where we create the window with two pointers and keep expanding until chars are unique
-- For keeping track of unique chars we can use a hashset so it ensures nothing is repeated
+- Since we are finding a contig sequence we know we can use a sliding window 
+- And that window has to be dynamic so expanding and shrinking since we are finding the longest and not a fixed size
+- Also since we know we are finding unique characters we can def utilize a hashset to check if that char has already occurred in the window
 
 Plan:
-- l = 0, hashset = {} and max = 0
-- run a loop over s with r
-    - now as long as s[r] keeps existing in hashset, we keep moving window from left side inwards, so remove s[l] from hashset and move l over
-- now once s[r] doesn't exist, append this new element and check if max needs to get updated
-- return max
-
-- So a key thing which kept confusing me was why do we run a while loop and not if, the reason is if we have abcbde, we get abc and when r gets on b now we want to start next string at c right so we keep deleting from hashset until left pointer comes to c, now left is on c, c is only element in hashset and r keeps moving and we get cbde
-
-Evaluate:
-- Was a bit tricky to be honest, got the logic couldn't get code, so 50% credit only
-- TC: O(n) looping once
-- SC: O(n) in worst if we store the whole s in hashset
+- have left pointer starting at 0
+- have hashset to keep track of unique chars
+- have a maxLen var starting at 0
+- run a loop over given string with r pointer
+- now the key point here is that i dont think we first add the element, we would have to check if it already exists
+- so we check if curr element at r exists in the hashset, if so remove it but we don't check with if we check with while because what if that element is somewhere in the middle? since we want it to stay contig, we keeping removing elements until we have removen the duplicate so within this while we keep removing element on left pointer and keep moving left forward to shrink the window from left
+- then in the end once we are out of while loop we know that element def does not exist anymore so we now append element at pointer r and continue
+- now we update maxLen accordingly
+- return maxLen out of the loop
 '''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l, hashset, maxlen = 0, set(), 0
+        l = 0
+        hashset = set()
+        maxLen = 0
         for r in range(len(s)):
             while s[r] in hashset:
                 hashset.remove(s[l])
                 l += 1
             hashset.add(s[r])
-            maxlen = max(maxlen, r - l + 1)
-        return maxlen
-        
+            maxLen = max(maxLen, r - l + 1)
+        return maxLen
