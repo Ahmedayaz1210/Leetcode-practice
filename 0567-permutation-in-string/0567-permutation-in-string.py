bd40ba186480b660571c5f9886255a4b18b2ce6e
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-03-15 14:51:15 -0400

    Time: 27 ms (40.01%), Space: 19.3 MB (80.04%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,87 +1,46 @@
 '''
-UNDERSTAND:
-- Given two strings s1 and s2 as input
-- Have to find at least 1 to all of permutations of s1 within s2
-- Remember s1's permutation has to be a contiguous sequence, letters can't break, meaning letters in s1 can't have letters which are not in s1, when a s1's permutation is present in s2, like example 2
-- Length of s1 and s2? 1 <= s1.length <= s2.length <= 10^4
-- s1 and s2 are only english letters? Yes Followup: Lower case? Yes
-- Time constraints for optimality?
-- Space constraints? 
-- Should we assume that the input strings s1 and s2 will always be valid, or should we handle any specific edge cases?
-- Can any letters in s1 repeat within itself?
+Understand:
+- Given two strings s1 and s2 we have to see if any permutation of s1 exists in s2 as a substring
+- Permutation means rearranging a string so it's still using the same characters 
+- So over here we have to see if in s2 we can find any sequence of s1
+- if it exists return true else false
+- check constraints
 
-MATCH:
-- Permutations is tricking us
-- Question is to basically find all occurrences of letters in s1 within s2 but as a contiguous sequence
-- Finding all occurrences? Can use a hash map
-- Looking for a contogious sequence window? Fixed sliding window technique
+Match:
+- I guess we can simplify this problem by thinking all we need to do is check if chars in s1 occur in s2 sequentially and if they occur with same frequency
+- So sliding window for sequential check and hash map for the frequency
 
-PLAN:
-- Create two hash maps both for s1 and s2
-- Put s1's letter and their corresponding occurrences values in hash map as a key value pair
-- Run a loop over s2 with a fixed size window of len(s1)
-- Append the current letters in the window inside s2's hashmap
-- See if occurrences for all letters match, if so return true
-- Else shrink window from left and take out that letter's occurrence from s2's hashmap and expand window from the right, appending that letter in s2 hash map
-- In the end return false because we never found a permutation
-
-My code:
-        s1_hashmap = {}
-        s2_hashmap = {}
-        for s in range(len(s1)):
-            s1_hashmap[s1[s]] = 1 + s1_hashmap.get(s1[s],0)
-        print(s1_hashmap)
-        
-        l = 0
-        for r in range(len(s2)):
-            
-            if s2[r] not in s1_hashmap:
-                if s2[l] in s2_hashmap:
-                    s2_hashmap[s2[l]] = s2_hashmap.get(s2[l]) - 1
-                l += 1
-            else:
-                s2_hashmap[s2[r]] = 1 + s2_hashmap.get(s2[r],0)
-                print(s2_hashmap)
-                if s1_hashmap == s2_hashmap:
-                    return True
-
-        if s1_hashmap == s2_hashmap:
-                return True
-        return False
-
-
-EVALUATE: 
-- My code was almost correct, once again had logic error from start so implementing one small missing thing got confusing
-- The trick was to add right pointer letter from the start and then check if any letter's value in s2 is greater than s1's if so take it out and move right
-- Another trick was rather than comparing the two hashmaps, since we were validating window, we had to match window size with s1's length
-- Another mistake I made was not using a while loop, I used if statement but with my logic if statement more sense:
-- Why a while Loop?
-- Multiple Characters Exceeding the Count: There can be cases where the count of a character exceeds its limit in s1_hashmap multiple times consecutively. The while loop ensures that the window is adjusted until all character counts are within acceptable limits.
-- Gradual Shrinking: The window might need to be shrunk multiple times in a row before it becomes valid again. An if statement would only handle one adjustment at a time, potentially missing multiple required adjustments.
-
-Time: O(n) n is length os s2
-Space complexity: In worst case, will have all letters so O(26) == O(1)
+Plan:
+- hashmap s1 = {}
+- hashmap s2 = {}
+- First we can loop over s1 and store letters with occurrences in the hashmap
+- l = 0
+- now we loop over s2 with pointer r
+    - we append or increment element in s2 hashmap
+    - run a while loop checking if element in s2 hashmap at r has higher occurrence than same element in s1 hashmap
+        - if so decrement left pointers occurrence from s2
+        - make sure u delete that element if its occurrence is 0 else u wont be able to compare hashmaps correctly
+        - no move l pointer
+    - check if curr window and s1 len matches and both hashmaps == each other: return true
+- return false
 '''
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1_hashmap = {}
         s2_hashmap = {}
-        
+
         for s in s1:
-            s1_hashmap[s] = 1 + s1_hashmap.get(s, 0)
-        
+            s1_hashmap[s] = s1_hashmap.get(s,0) + 1
+
         l = 0
         for r in range(len(s2)):
-            s2_hashmap[s2[r]] = 1 + s2_hashmap.get(s2[r], 0)
-            
-            while s2_hashmap.get(s2[r], 0) > s1_hashmap.get(s2[r], 0):
+            s2_hashmap[s2[r]] = s2_hashmap.get(s2[r],0) + 1
+
+            while s2_hashmap.get(s2[r],0) > s1_hashmap.get(s2[r],0):
                 s2_hashmap[s2[l]] -= 1
                 if s2_hashmap[s2[l]] == 0:
-                    del s2_hashmap[s2[l]]  
+                    del s2_hashmap[s2[l]]
                 l += 1
-            
-            if (r - l + 1) == len(s1):
-                if s1_hashmap == s2_hashmap:
-                    return True
-        
+            if (r - l + 1) == len(s1) and s1_hashmap == s2_hashmap:
+                return True
         return False
