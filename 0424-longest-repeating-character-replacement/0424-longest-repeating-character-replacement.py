fbd3052b0a427369d1c371c904f1290cef7b823d
Ahmed Ayaz <114943142+Ahmedayaz1210@users.noreply.github.com>
2026-03-15 12:04:30 -0400

    Time: 159 ms (19.63%), Space: 19.7 MB (79.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,50 +1,40 @@
 '''
-Understand: 
-- Given two inputs string s and k int, you can perform k operations on s, operation here means that replacing any char from s to an uppercase alphabet and you can do this k times 
-- Asked to find the longest chain of same uppercase chars, so looking at example 1 we replace both As with 2 Bs or both Bs with two As so we can get either chain of all As or all Bs but in this case both will be len 4 so doesn't matter - In the end we return the longest sequence of same char 
-- Does s only have uppercase alphas? yes - How long is s? 1 to 10^5 - how big is k? 0 to len of s
+Understand:
+- Given a string s and and int k, we have to find longest substring in s so that all chars in s are the same, in this case it can be only uppercase english letters
+- But we get k amount of replacements, so in our string we can change any k amount of characters
+- So kind of an analogy that whatever is the most occurring letter, change k letters to that one to keep the sequence going
+- So we can't have any empty string there has to be at least one letter but k can be 0
+- Check constraints for futher explanation
 
 Match:
-- So we definitely need a hashmap to keep track of occurences of each alphabet.
-- I think logic here is we will have a dynamic sliding window where right pointer loops the list and left moves accordingly. 
-- I think the logic is that we keep checking the window size and compare it with keeping track of highest frequency character, when we subtract we get the number of elements which need to get changed and if that number exceeds k that's when window becomes invalid for example 2:
-when window is "AABAB" window size is 5, A is occuring the most with 3 A's, 5-3 = 2 elements need to get changed but we can only change one so that's invalid now
-- And if it any point it doesn't satisfy we keep running the while loop and update left accordingly.
+- So a sequential substring tells me that I can use sliding window approach where we can have two pointers creating a window of the longest substring
+- Since we need to keep track of all the elements in the window to see what needs to be replaced we can use a hashmap because we kind of need to know highest occurring one and how we can use k tries to change other letters
 
 Plan:
-- left pointer at 0
-- Hashmap = {}
-- longest_window = 0
-- for r in range(len(s)):
-    - add the right element to hashmap
-    - window_size = r - l + 1
-    - max_freq = i dont know how to get max value from hashmap right now
-    - if window_size - max_freq > k:
-        - subtract l occurence from hashmap and if 0 then element gets deleted
+- Left pointer starts at 0
+- hashmap = {}
+- longest sub = 0
+- run loop over string with pointer r
+    - maybe we can start by simply appending the element at r into hash map
+    - now we check if the curr window size - highest occurring letter from hashmap is > k, if so we know that we are out of k tries 
+        - now we subtrack l element by 1 from hashmap
         - l += 1
-    - longest_window = max(longest_window, window_size)
-return longest_window
+    - we update the highest window size
+- return longest sub
 
-Evaluate:
-- Got the logic but had a lot of mistakes, need to do more easys before jumping into mediums
-- SC: O(26) or O(1) in worse case
-- TC: O(n) looping once
 '''
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l = 0
         hashmap = {}
-        longest_window = 0
-        max_freq = 0
+        longestSub = 0
 
         for r in range(len(s)):
-            hashmap[s[r]] = hashmap.get(s[r], 0) + 1 #if doesnt exist do 0 else increment
-            window_size = r - l + 1
-            max_freq = max(max_freq, hashmap[s[r]])
-
-            if window_size - max_freq > k:
-                hashmap[s[l]] -= 1
+            hashmap[s[r]] = hashmap.get(s[r], 0) + 1
+            currWinSize = r - l + 1
+            highestElement = hashmap.get(max(hashmap, key=hashmap.get))
+            if currWinSize - highestElement > k:
+                hashmap[s[l]] = hashmap.get(s[l], 0) - 1
                 l += 1
-
-            longest_window = max(longest_window, r - l + 1)
-        return longest_window
+            longestSub = max(longestSub, r - l + 1)
+        return longestSub
